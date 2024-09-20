afac29c0148d5fe37cc0a8b2b692fafbb20df706
Lucas Stasiak <108033145+StasiakLucas@users.noreply.github.com>
2024-09-20 12:12:04 -0300

    20/09/24

---
+++ main.js
@@ -0,0 +1,12 @@
+alert("Oi Lucas");
+console.log("Oi João");
+
+var titulo = document.querySelector("h1");
+titulo.textContent = "Lucas Nutrição";
+
+var trPaulo = document.querySelector("#paulo");
+
+console.log(trPaulo);
+
+var tdPeso = trPaulo.querySelector(".info-peso");
+tdPeso.textContent = "150";
