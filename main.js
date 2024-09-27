1e99f12c207d6951628c82c521c0a5624faca61c
Lucas Stasiak <108033145+StasiakLucas@users.noreply.github.com>
2024-09-27 12:11:17 -0300

    codigo atualizado

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Lucas Nutrição";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(var i = 0; i < 5; i++) {
+  console.log(pacientes[i]);
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+  var tdPeso = pacientes[i].querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  var tdAltura = pacientes[i].querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  var tdImc = pacientes[i].querySelector(".info-imc");
+
+  var pesoEhValido = true;
+  var alturaEhValida = true;
+
+  if(peso < 0 || peso > 600) {
+    tdImc.textContent = "Peso inválido!";
+    pesoEhValido = false;
+  }
+
+  if(altura < 0 || altura > 2.80){
+    tdImc.textContent = "Altura inválida!"
+    alturaEhValida = false;
+  }
+
+  if(pesoEhValido && alturaEhValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+  }
+}
